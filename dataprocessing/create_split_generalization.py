e4cc52304ba955ff04d0c9e25ad417f3ee3fdf58
Xianghui Yang <xianghui.yang@sydney.edu.au>
2023-04-01 17:35:06 +1100

    Update create_split_generalization.py

--- dataprocessing/create_split_generalization.py
+++ dataprocessing/create_split_generalization.py
@@ -6,6 +6,8 @@ from collections import defaultdict
 
 shapenetv1_dir = "/home/xianghui_yang/data/ShapeNet/ShapeNetCore.v1"
 save_dir = "/home/xianghui_yang/data/NVF_data"
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 # cls_idx_list = ["02958343", "03001627", "02691156", "03691459", "02828884", "04530566", "03636649", "04379243"] # [car, chair, plane, speaker, bench, watercraft, lamp, table]
 cls_idx_list = ["04379243"]
 for cls_idx in cls_idx_list:
